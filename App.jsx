0767b84f2ad4c8a5027d79319a4d5f59ffa4756a
KMV51 <57423048+KMV51@users.noreply.github.com>
2023-10-14 15:29:22 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Menu from './Menu.jsx'
+
+function App() {
+
+  return(
+    <>
+        <Header></Header>
+        <Menu></Menu>
+        <Menu></Menu>
+        <Menu></Menu>
+        <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ Menu.jsx
@@ -0,0 +1,15 @@
+function Menu()
+{
+
+    const food1 = "Sushi";
+    const foos2 = "Sashimi";
+    return(
+        <ul>
+            <li>California Roll</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Menu
